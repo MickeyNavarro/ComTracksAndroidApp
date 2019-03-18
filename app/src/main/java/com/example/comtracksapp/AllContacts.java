ef8bdf2d9422fc1dfb07301bffaec2bc22249e95
Mickey Navarro <37810413+MickeyNavarro@users.noreply.github.com>
2019-03-18 13:36:14 -0700

    second commit

---
+++ app/src/main/java/com/example/comtracksapp/AllContacts.java
@@ -0,0 +1,13 @@
+package com.example.comtracksapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AllContacts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_contacts);
+    }
+}
